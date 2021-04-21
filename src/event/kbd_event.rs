500e0c2f0fefa8420fede8f5b70aaa96b421d527
ldg <ldg@LDG.LDG>
2021-04-21 02:55:51 +0100

    My assynchronous doer has been implemented

--- src/event/kbd_event.rs
+++ src/event/kbd_event.rs
@@ -1,37 +1,87 @@
-use std::{collections::HashMap, sync::Mutex};
+use std::{
+    collections::HashMap,
+    debug_assert, result,
+    sync::{Arc, Mutex},
+    thread,
+    cell::RefCell,
+};
 
-mod kbd_event_handler;
-use kbd_event_handler::KbdEventHandler;
+use super::kbd_event_handler::{self, KbdEventHandler,};
+pub use super::kbd_event_handler::{
+    KeyFlags,
+    KEY_RELEASED,
+    KEY_PRESSED,
+    KEY_REPEATED,
+};
 
 pub type KbdEvent = Event;
 
-struct Event { 
-    controller: Mutex<kbd_event_handler::Controller>;
+pub struct Event {
+    controller: Arc<Mutex<kbd_event_handler::Controller>>,
+    kbd_event_handler_child: Option<thread::JoinHandle<()>>,
 }
 
-/* No support for held keys at the moment. That would require an array 
+/* No support for held keys at the moment. That would require an array
 * containing the state of each key, for now use pressed or released and
 * manage the state youself.
 */
 impl KbdEvent {
-    pub fn new() -> KbdEvent{
+    pub fn new() -> KbdEvent {
+        let controller = kbd_event_handler::Controller {
+            actions: HashMap::new(),
+            done: false,
+        };
         Event {
-            Mutex::new(
-                kbd_event_handler::Controller {
-                    actions: Mutex::new(Hasmap::new()),
-                    done: false,
-                })
+            controller: Arc::new(Mutex::new(controller)),
+            kbd_event_handler_child: None,
         }
     }
 
-    pub fn add_event<F: 'static>(&mut self, key: u16, f: F) where F: FnOnce(){
+    pub fn add_event<F: 'static>(&mut self, key: u16, f: F, flags: KeyFlags)
+    where
+        F: Fn() + Sync + Send,
+    {
         self.controller
             .lock()
             .unwrap()
             .actions
-            .insert(key, Box::new(f));
+            .insert(key, (Box::new(f), RefCell::new(flags)));
     }
 
+    pub fn alter_event(&mut self, key: u16, flags: KeyFlags) {
+        debug_assert!(flags == KEY_PRESSED || flags == KEY_REPEATED 
+                      || flags == KEY_RELEASED); 
+        self.controller
+            .lock()
+            .unwrap()
+            .actions
+            .get(&key)
+            .map(|f| *f.1.borrow_mut() = flags);
 
+    }
+    
+    pub fn remove_event(&mut self, key: u16) {
+        self.controller
+            .lock()
+            .unwrap()
+            .actions
+            .remove(&key);
+    }
+
+    pub fn start(&mut self) -> std::io::Result<()> {
+        let child_controller = Arc::clone(&self.controller);
+        let kbd_event_handler = KbdEventHandler::new(child_controller)?;
+
+        let handle = thread::spawn(move || kbd_event_handler.start());
+        self.kbd_event_handler_child = Some(handle);
+        Ok(())
+    }
+
+    pub fn stop(&mut self) {
+        //debug_assert!(self.kbd_event_handler_child.map_or(false, |_| true));
+        self.controller.lock().unwrap().done = true;
+        self.kbd_event_handler_child
+            .take()
+            .map(|handle| handle.join().unwrap());
+    }
 }
-
--- src/event/kbd_event_handler.rs
+++ src/event/kbd_event_handler.rs
@@ -1,57 +1,132 @@
-use std::{fs::File, io::Read, mem, collections::HashMap, sync::Mutex};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::Read,
+    mem,
+    sync::{Arc, Mutex},
+    cell::RefCell,
+    thread,
+    assert,
+};
+
 use libc::input_event;
 
 pub use input_event_codes::*;
 
-const KEY_RELEASED: i32 = 0;
-const KEY_PRESSED : i32 = 1;
-const KEY_REPEATED: i32 = 2;
+const KEY_RELEASE: i32 = 0;
+const KEY_PRESS: i32 = 1;
+const KEY_REPEAT: i32 = 2;
+
+#[macro_export]
+macro_rules! BIT {
+    ($bit:expr) => {
+        (1 << $bit)
+    };
+}
+
+pub type KeyFlags = u8;
+pub const KEY_RELEASED: KeyFlags = BIT!(0);
+pub const KEY_PRESSED:  KeyFlags = BIT!(1);
+pub const KEY_REPEATED: KeyFlags = BIT!(2);
+
 
 pub type KbdEventHandler = Handler;
 
-struct Handler {
-    key_press : input_event,
-    kbd_file  : File,
-    controller: Mutex<Controller>,
+pub struct Handler {
+    key_press: input_event,
+    kbd_file: File, /* TODO Find this file manually */
+    /* Arc-Mutex needed to share controller between threads */
+    controller: Arc<Mutex<Controller>>, 
+    handles: Vec<thread::JoinHandle<Option<()>>>, /* Handles on the Child threads */
 }
 
+/* Must be boxed since the trait's size in unknown */
+type Task = Box<dyn Fn() + Sync + Send>;
+type TaskMap = HashMap<u16, (Task, RefCell<KeyFlags>)>;
+
 pub struct Controller {
-    actions: HashMap<u16, Box<dyn FnOnce()>>,
-    done   : bool,
+    pub actions: TaskMap,
+    pub done: bool,
 }
 
 impl KbdEventHandler {
-
-    pub fn new(controller: Mutex<Controller>) 
-        -> std::io::Result<KbdEventHandler>{
-        Ok (
-            Handler {
-                key_press : unsafe { mem::zeroed() },
-                kbd_file  : File::open("/dev/input/event0")?, 
-                controller: controller,
-            }
-        )
+    pub fn new(controller: Arc<Mutex<Controller>>) -> std::io::Result<KbdEventHandler> {
+        Ok(Handler {
+            key_press: unsafe { mem::zeroed() },
+            kbd_file: File::open("/dev/input/event0")?,
+            controller,
+            handles: Vec::new(),
+        })
     }
-    fn poll(&mut self) -> std::io::Result<()>{
-        let mut buf : [u8; mem::size_of::<input_event>()] = 
-            unsafe { mem::zeroed() };
+    /* This function is to run in another thread so it takes full ownership of
+     * self */
+    pub fn start(mut self) {
+        while !self.controller.lock().unwrap().done {
+            self.poll().unwrap(); /* polling should never fail. */
+            let key_code = self.key_press.code;
+            let key_value = self.key_press.value;  
+            /* Continues if the key_press is not in the map. Stops creation 
+             * of a needless thread. */
+            if self.key_press.type_ != EV_KEY {continue;}
+            if let None = &self.controller
+                               .lock()
+                               .unwrap()
+                               .actions
+                               .get(&key_code) 
+                               {continue;}
+            /* Clone of controller needed for each child_thread */
+            let child_controller = Arc::clone(&self.controller);
+            /* Creates the thread. Still has to check map for the closure
+             * since another thread may have updated the actions HashMap */
+            let handle = thread::spawn(move || {
+                child_controller
+                    .lock()
+                    .unwrap()
+                    .actions
+                    .get(&key_code)
+                    .map(|f| 
+                         if (key_val_to_flag(key_value) & *f.1.borrow()) > 0 { 
+                             f.0() 
+                         })
+            });
+            self.handles.push(handle);
+        }
+        /* Must wait for all child threads to finish */
+        for handle in self.handles {
+            handle.join().unwrap();
+        }
+    }
+    
+    /* Reads keyboard device file */
+    fn poll(&mut self) -> std::io::Result<()> {
+        let mut buf: [u8; mem::size_of::<input_event>()] = unsafe { mem::zeroed() };
         self.kbd_file.read(&mut buf)?;
         self.key_press = unsafe { mem::transmute(buf) };
         Ok(())
     }
-    fn pressed(&self, key : u16) -> bool {
-       return self.key_press.type_ == EV_KEY
-              && self.key_press.code == key 
-              && self.key_press.value == KEY_PRESSED
-    }
-    fn released(&self, key : u16) -> bool {
-       return self.key_press.type_ == EV_KEY
-              && self.key_press.code == key 
-              && self.key_press.value == KEY_RELEASED;
-    }
-    fn repeated(&self, key : u16) -> bool {
-       return self.key_press.type_ == EV_KEY
-              && self.key_press.code == key 
-              && self.key_press.value == KEY_REPEATED;
+
+   // fn pressed(&self, key: u16) -> bool {
+   //     return self.key_press.type_ == EV_KEY
+   //         && self.key_press.code == key
+   //         && self.key_press.value == KEY_PRESSED;
+   // }
+   // fn released(&self, key: u16) -> bool {
+   //     return self.key_press.type_ == EV_KEY
+   //         && self.key_press.code == key
+   //         && self.key_press.value == KEY_RELEASED;
+   // }
+   // fn repeated(&self, key: u16) -> bool {
+   //     return self.key_press.type_ == EV_KEY
+   //         && self.key_press.code == key
+   //         && self.key_press.value == KEY_REPEATED;
+   // }
+}
+
+fn key_val_to_flag(val: i32) -> u8 {
+    match val {
+        KEY_RELEASE => KEY_RELEASED,
+        KEY_PRESS => KEY_PRESSED,
+        KEY_REPEAT => KEY_REPEATED,
+        _ => 0,
     }
 }
